60cf2478ff95cfa72700733cf8b0cde618155fe1
Courtney818 <courtneyerickson818@yahoo.com>
2022-05-25 16:26:09 -0500

    app bar

--- src/App.vue
+++ src/App.vue
@@ -1,36 +1,43 @@
 <template>
   <v-app>
-    <v-app-bar app color="primary" dark>
+    <v-app-bar app color="black" dark>
       <div class="d-flex align-center">
-        <v-img
-          alt="Vuetify Logo"
-          class="shrink mr-2"
+        <v-icon
+          alt="Billiard Tracker Logo"
+          class="shrink mr-3"
           contain
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"
           transition="scale-transition"
           width="40"
-        />
+        >
+          mdi-billiards
+        </v-icon>
 
-        <v-img
-          alt="Vuetify Name"
-          class="shrink mt-1 hidden-sm-and-down"
+        <div
+          alt="Adam's Billiard Tracker"
+          class="shrink font-weight-bold hidden-sm-and-down"
           contain
           min-width="100"
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-name-dark.png"
           width="100"
-        />
+        >
+          Billiard Tracker
+        </div>
       </div>
 
       <v-spacer></v-spacer>
 
-      <v-btn
-        href="https://github.com/vuetifyjs/vuetify/releases/latest"
-        target="_blank"
-        text
-      >
-        <span class="mr-2">Latest Release</span>
-        <v-icon>mdi-open-in-new</v-icon>
-      </v-btn>
+      <v-menu bottom left>
+        <template v-slot:activator="{ on, attrs }">
+          <v-btn dark icon v-bind="attrs" v-on="on">
+            <v-icon>mdi-dots-vertical</v-icon>
+          </v-btn>
+        </template>
+
+        <v-list>
+          <v-list-item v-for="(item, i) in items" :key="i">
+            <v-list-item-title>{{ item.title }}</v-list-item-title>
+          </v-list-item>
+        </v-list>
+      </v-menu>
     </v-app-bar>
 
     <v-main>
@@ -49,7 +56,9 @@ export default {
     HomePage,
   },
 
-  data: () => ({}),
+  data: () => ({
+    items: [{ title: 'Dashboard' }],
+  }),
 };
 </script>
 
